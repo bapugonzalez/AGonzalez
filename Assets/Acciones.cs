700574cb49424a7f89d7d37743255bee28864dcf
bapugonzalez <bapu76@hotmail.com>
2023-01-28 14:25:04 -0500

    Initial commit

---
+++ Assets/Acciones.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Acciones : MonoBehaviour
+{
+	public TextMeshProUGUI posibles, operacion, entrada1, entrada2, resultado;
+	public int puntos = 0;
+	public GameObject [] botones;
+	
+	void Pregunta1()
+	{
+		posibles.text = "1, 8, 5, 3";
+		operacion.text = "+";
+		resultado.text = "11";
+		
+		if(entrada1.text == "8" && entrada2.text == "3")
+		{
+			puntos++;
+		}
+		else if(entrada1.text == "3" && entrada2.text == "8")
+		{
+			puntos++;
+		}
+		botones[0].SetActive(false);
+		botones[1].SetActive(true);
+		
+		//Inicializar Pregunta 2
+	}
+}
